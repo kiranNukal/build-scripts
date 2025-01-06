d5299354e3990e6d8834770c0a0523e68e99b9eb
kiranNukal <sai.kiran.nukala@ibm.com>
2025-01-06 15:13:43 +0530

    Update pyarrow_ubi_9.3.sh

--- p/pyarrow/pyarrow_ubi_9.3.sh
+++ p/pyarrow/pyarrow_ubi_9.3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 # -----------------------------------------------------------------------------
 #
-# Package       : pyarrow
-# Version       : 11.0.0
-# Source repo : https://github.com/apache/arrow.git
+# Package       : xgboost
+# Version       : 1.6.2
+# Source repo :  https://github.com/dmlc/xgboost
 # Tested on     : UBI:9.3
 # Language      : Python
 # Travis-Check  : True
 # Script License: Apache License, Version 2 or later
-# Maintainer    : Sai Kiran Nukala <sai.kiran.nukala@ibm.com>
+# Maintainer : Sai Kiran Nukala <sai.kiran.nukala@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on the given
 # platform using the mentioned version of the package.
@@ -17,119 +17,66 @@
 # contact the "Maintainer" of this script.
 #
 # -----------------------------------------------------------------------------
-set -e
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+ 
 # Variables
-PACKAGE_NAME=pyarrow
-PACKAGE_VERSION=${1:-apache-arrow-11.0.0}
-PACKAGE_URL=https://github.com/apache/arrow.git
-PACKAGE_DIR=arrow
+PACKAGE_NAME=xgboost
+PACKAGE_VERSION=${1:-v1.6.2}
+PACKAGE_URL=https://github.com/dmlc/xgboost
+PACKAGE_DIR=xgboost/python-package
+OUTPUT_FOLDER="$(pwd)/output"
  
-echo "Installing dependencies..."
+echo "PACKAGE_NAME: $PACKAGE_NAME"
+echo "PACKAGE_VERSION: $PACKAGE_VERSION"
+echo "PACKAGE_URL: $PACKAGE_URL"
+echo "OUTPUT_FOLDER: $OUTPUT_FOLDER"
+ 
 # Install dependencies
-yum install -y git wget gcc gcc-c++ python python3-devel python3 python3-pip openssl-devel cmake
-echo "Dependencies installed."
+echo "Installing dependencies..."
+yum install -y git wget gcc gcc-c++ python python3-devel python3 python3-pip openssl-devel cmake openblas-devel gcc-gfortran
+pip install numpy packaging pathspec pluggy scipy trove-classifiers pytest wheel build
  
-echo "Cloning the repository..."
 # Clone the repository
-git clone $PACKAGE_URL
-cd $PACKAGE_DIR
-git checkout $PACKAGE_VERSION
-git submodule update --init
-echo "Repository cloned and checked out to version $PACKAGE_VERSION."
+echo "Cloning the repository..."
+mkdir -p output
+git clone -b ${PACKAGE_VERSION} --recursive $PACKAGE_URL
+cd $PACKAGE_NAME
+export SRC_DIR=$(pwd)
+echo "SRC_DIR: $SRC_DIR"
  
-echo "Setting test data paths..."
-# Set test data paths
-export PARQUET_TEST_DATA="${PWD}/cpp/submodules/parquet-testing/data"
-export ARROW_TEST_DATA="${PWD}/testing/data"
-echo "Test data paths set."
- 
-echo "Applying fixes for nogil placement and rvalue issues..."
-# Apply fixes for nogil placement and rvalue issues
-sed -i -E 's/(nogil)(.*)(except[^:]*)/\2\3 \1/' python/pyarrow/error.pxi
-sed -i -E 's/(nogil)(.*)(except[^:]*)/\2\3 \1/' python/pyarrow/includes/libarrow.pxd
-sed -i -E 's/(nogil)(.*)(except[^:]*)/\2\3 \1/' python/pyarrow/lib.pxd
-# Replace rvalue references '&&' with lvalue references '&'
-sed -i -E 's/\&\&/\&/g' python/pyarrow/error.pxi
-sed -i -E 's/\&\&/\&/g' python/pyarrow/includes/libarrow.pxd
-sed -i -E 's/\&\&/\&/g' python/pyarrow/lib.pxd
-sed -i -E 's/\&\&/\&/g' python/pyarrow/includes/libarrow_fs.pxd
-echo "Fixes applied."
- 
-echo "Preparing for build..."
-# Prepare for build
-cd ..
-pip install -r arrow/python/requirements-build.txt
-pip install -r arrow/python/requirements-test.txt
-mkdir dist
-export ARROW_HOME=$(pwd)/dist
-export LD_LIBRARY_PATH=$ARROW_HOME/lib:$LD_LIBRARY_PATH
-export CMAKE_PREFIX_PATH=$ARROW_HOME:$CMAKE_PREFIX_PATH
-echo "Build preparation completed."
- 
-echo "Building Arrow C++ libraries..."
-# Build Arrow C++ libraries
-mkdir arrow/cpp/build
-cd arrow/cpp/build
-cmake -DCMAKE_INSTALL_PREFIX=$ARROW_HOME \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_BUILD_TYPE=Debug \
-    -DARROW_BUILD_TESTS=ON \
-    -DARROW_COMPUTE=ON \
-    -DARROW_CSV=ON \
-    -DARROW_DATASET=ON \
-    -DARROW_FILESYSTEM=ON \
-    -DARROW_HDFS=ON \
-    -DARROW_JSON=ON \
-    -DARROW_PARQUET=ON \
-    -DARROW_WITH_BROTLI=ON \
-    -DARROW_WITH_BZ2=ON \
-    -DARROW_WITH_LZ4=ON \
-    -DARROW_WITH_SNAPPY=ON \
-    -DARROW_WITH_ZLIB=ON \
-    -DARROW_WITH_ZSTD=ON \
-    -DPARQUET_REQUIRE_ENCRYPTION=ON \
-    ..
+# Build xgboost cpp artifacts
+echo "Building xgboost cpp artifacts..."
+cd ${SRC_DIR}
+mkdir -p build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${OUTPUT_FOLDER} ..
 make -j$(nproc)
-make install
-echo "Arrow C++ libraries built and installed."
  
-echo "Building PyArrow..."
-# Build PyArrow
-cd ../..
-cd python
-export PYARROW_WITH_PARQUET=1
-export PYARROW_WITH_DATASET=1
-export PYARROW_PARALLEL=4
-pip install pytest==6.2.5
-pip install "numpy<2.0"
-pip install --upgrade setuptools wheel
-pip install wheel hypothesis pytest-lazy-fixture pytz
-CMAKE_PREFIX_PATH=$ARROW_HOME python setup.py build_ext --inplace
+LIBDIR=${OUTPUT_FOLDER}/lib
+INCDIR=${OUTPUT_FOLDER}/include
+BINDIR=${OUTPUT_FOLDER}/bin
+SODIR=${LIBDIR}
+XGBOOSTDSO=libxgboost.so
+
+mkdir -p ${LIBDIR} ${INCDIR}/xgboost ${BINDIR} || true
+cp ${SRC_DIR}/lib/${XGBOOSTDSO} ${SODIR}
+cp -Rf ${SRC_DIR}/include/xgboost ${INCDIR}/
+cp -Rf ${SRC_DIR}/rabit/include/rabit ${INCDIR}/xgboost/
+cp -f ${SRC_DIR}/src/c_api/*.h ${INCDIR}/xgboost/
+cd ../../
  
-echo "Installing PyArrow Python package..."
-# Install the generated Python package
-if ! CMAKE_PREFIX_PATH=$ARROW_HOME python3 setup.py install; then
-    echo "------------------$PACKAGE_NAME::Python package installation failed-------------------------"
+# Build xgboost python artifacts and wheel
+echo "Building xgboost Python artifacts and wheel..."
+cd "$(pwd)/$PACKAGE_DIR"
+echo "Current directory: $(pwd)"
+ 
+if ! (python3 setup.py install); then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail | Install_Fails"
     exit 1
 fi
-echo "PyArrow Python package installed."
  
-export PYTEST_PATH=$(pwd)/pyarrow
-export PYTEST_ADDOPTS="-k 'not test_cython and not test_extension_type' --deselect=pyarrow/tests/test_extension_type.py --deselect=pyarrow/tests/test_compute.py --deselect=pyarrow/tests/test_ipc.py --deselect=pyarrow/tests/test_pandas.py --deselect=pyarrow/tests/parquet/test_dataset.py"
- 
-echo "Running Python tests..."
-# Run Python tests
-if ! python3 -m pytest $PYTEST_PATH; then
-    echo "------------------$PACKAGE_NAME::Python_Test_fails-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Fail | Python_Test_Fails"
-    exit 2
-else
-    echo "------------------$PACKAGE_NAME::Build_and_All_Tests_success-------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME | $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub | Pass | Build_and_All_Tests_Success"
-    exit 0
-fi
+echo "Build and installation completed successfully."
